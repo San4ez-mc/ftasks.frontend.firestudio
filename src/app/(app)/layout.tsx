31a8f09229ac7e5e2fe6f4a438dce6542f211042
Олександр Мацук <olexandrmatsuk@gmail.com>
2025-09-05 15:05:44 +0000

    верни, будь ласка, хедер, меню, сайтбари і футер

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,5 @@
-import AppShell from '@/components/layout/app-shell';
 import type { ReactNode } from 'react';
 
 export default function AppLayout({ children }: { children: ReactNode }) {
-  return <AppShell>{children}</AppShell>;
+  return <>{children}</>;
 }
